6d20d822395df9c7ab8e75ba0b9b5e98044c2bc9
breno.polanski <breno.polanski@gmail.com>
2015-11-15 19:12:45 -0300

    Create initial authentication with GitHub API

--- controllers/login.js
+++ controllers/login.js
@@ -1,9 +1,16 @@
 'use strict';
 
+var loadConfig = require('../utils/loadConfig');
+
 module.exports = function(app) {
 	var LoginController = {
 		index: function(req, res) {
-			res.render('login/index');
+			var config = loadConfig();
+			var url = 'https://github.com/login/oauth/authorize?client_id=' + config.oauth_client_id + '&redirect_uri=' + config.oauth_redirect_uri + '&scope=' + config.oauth_scope + '';
+			var params = {
+				url: url
+			};
+			res.render('login/index', params);
 		}
 	};
 
