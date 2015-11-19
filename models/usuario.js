d23e16ede01249e256285366289fb444952e0f14
breno.polanski <breno.polanski@gmail.com>
2015-11-18 22:32:50 -0300

    Update user schema

--- models/usuario.js
+++ models/usuario.js
@@ -3,6 +3,18 @@
 module.exports = function(app) {
 	var Schema = require('mongoose').Schema;
 
+	var coffee = Schema({
+		amount : { type: Integer },
+		flavor : { type: String },
+		date   : { type: String, required: true }
+	});
+
+	var guest = Schema({
+		amount : { type: Integer },
+		flavor : { type: String },
+		date   : { type: String, required: true }
+	});
+
 	var usuario = Schema({
 		name            : { type: String, required: true },
 		email           : { type: String, required: true, index: { unique: true } },
@@ -12,6 +24,11 @@ module.exports = function(app) {
 		github_id       : { type: String, required: true },
 		github_username : { type: String, required: true },
 		profile_image   : { type: String, required: true },
-		github_url      : { type: String, required: true }
+		github_url      : { type: String, required: true },
+		date            : { type: String, required: true },
+		coffees         : [coffee],
+		guests          : [guest]
 	});
+
+	return db.model('usuarios', usuario);
 };
