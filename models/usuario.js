545491d876229948cf9d028ebd82bf96e1932903
breno.polanski <breno.polanski@gmail.com>
2015-11-18 22:00:21 -0300

    Add user schema

--- models/usuario.js
+++ models/usuario.js
@@ -2,4 +2,16 @@
 
 module.exports = function(app) {
 	var Schema = require('mongoose').Schema;
+
+	var usuario = Schema({
+		name            : { type: String, required: true },
+		email           : { type: String, required: true, index: { unique: true } },
+		company         : { type: String },
+		blog            : { type: String },
+		location        : { type: String },
+		github_id       : { type: String, required: true },
+		github_username : { type: String, required: true },
+		profile_image   : { type: String, required: true },
+		github_url      : { type: String, required: true }
+	});
 };
