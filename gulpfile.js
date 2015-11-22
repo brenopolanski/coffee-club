47d44d082450c1a4fdd29d70cf9d9d01441c3b98
breno.polanski <breno.polanski@gmail.com>
2015-11-22 19:46:51 -0300

    Update UI

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
+var gulp   = require('gulp');
+var stylus = require('gulp-stylus');
 
+gulp.task('stylus', function() {
+	return gulp.src('./src/styl/style.styl')
+			.pipe(stylus())
+			.pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('default', ['stylus'], function() {
+	gulp.watch('./src/styl/style.styl', ['stylus']);
+});
