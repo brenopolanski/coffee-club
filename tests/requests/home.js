8fe68b5a70a10d742d39992714f3b80ca620f88e
breno.polanski <breno.polanski@gmail.com>
2015-12-04 22:40:24 -0300

    Add other test: Must to go for route / when doing GET /exit

--- tests/requests/home.js
+++ tests/requests/home.js
@@ -1,14 +1,23 @@
 'use strict';
 
-var app     = require('../../app');
+var server  = require('../../server');
 var should  = require('should');
-var request = require('supertest')(app);
+var request = require('supertest')(server);
 
-describe('No controller home', function() {
+describe('The home controller', function() {
 	it('Must return status 200 when doing GET', function(done) {
 		request.get('/')
-			.end(function(err, res) {
-				res.status.should.eql(200);
+		.end(function(err, res) {
+			res.status.should.eql(200);
+			done();
+		});
+	});
+
+	it('Must to go for route / when doing GET /exit', function(done) {
+		request.get('/exit')
+		.end(function(err, res) {
+			res.headers.location.should.eql('/');
+			done();
 		});
 	});
 });
