3e91721ca96c6dd2b394969175e5cce6f3a4f129
breno.polanski <breno.polanski@gmail.com>
2015-12-05 23:04:10 -0300

    Add Singleton in MongoDB connection

--- libs/db_connect.js
+++ libs/db_connect.js
@@ -2,21 +2,24 @@
 
 module.exports = function() {
 	var mongoose = require('mongoose');
-	var env_url = {
-		'test': 'mongodb://localhost/coffeeclub_test',
-		'development': 'mongodb://localhost/coffeeclub'
-	};
-	var url = 
+	var config   = require('../config.json');
+	var env = 
 		process.env.MONGOLAB_URI || 
 		process.env.MONGOHQ_URL ||
-		env_url[process.env.NODE_ENV || 'development'];
+		process.env.NODE_ENV || 'development';
+	var url = config.mongodb[env];
+	var singleConnection;
 
-	return mongoose.connect(url, function(err, res) {
-		if (err) { 
-			console.log('ERROR connecting to: ' + url + '. ' + err);
-		} 
-		else {
-			console.log('Succeeded connected to: ' + url);
-		}
-	});
+	if (!singleConnection) {
+		singleConnection = mongoose.connect(url, function(err, res) {
+			if (err) { 
+				console.log('ERROR connecting to: ' + url + '. ' + err);
+			} 
+			else {
+				console.log('Succeeded connected to: ' + url);
+			}
+		});
+	}
+
+	return singleConnection;
 };
